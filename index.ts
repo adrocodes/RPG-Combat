03ac148507ea8652b9879b7e3b58ad1617c06703
adrocodes <adrocodes@users.noreply.github.com>
2023-06-02 21:48:21 +1000

    iteration 4

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+export class Faction {
+  constructor(public name: string) {}
+}
+
 export class Character {
   static _id: number = 0;
 
@@ -6,15 +10,22 @@ export class Character {
   public alive: boolean = true;
   public id: number;
   public range: number = 0;
+  public factions: Faction[] = [];
 
   constructor() {
     this.id = Character._id++;
+  }
+
+  private isAllied(target: Character) {
+    return this.factions.some((faction) => target.factions.includes(faction));
   }
 
   public attack(target: Character, amount: number, distance: number = 0) {
     if (this.id === target.id) return;
 
     if (this.range < distance) return;
+
+    if (this.isAllied(target)) return;
 
     let multipler = 1;
 
@@ -37,6 +48,24 @@ export class Character {
 
     this.health += amount;
     this.health = Math.min(this.health, 1000);
+  }
+
+  public healAllies(target: Character, amount: number) {
+    if (!this.isAllied(target)) return;
+
+    target.heal(amount)
+  }
+
+  public joinFaction(faction: Faction) {
+    const inFaction = this.factions.find((f) => f.name === faction.name);
+
+    if (!inFaction) {
+      this.factions.push(faction);
+    }
+  }
+
+  public leaveFaction(faction: Faction) {
+    this.factions = this.factions.filter((f) => f.name !== faction.name);
   }
 }
 
