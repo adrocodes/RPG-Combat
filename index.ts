d4902f1185491d3378b6922da8df1c8561188940
adrocodes <adrocodes@users.noreply.github.com>
2023-06-03 19:38:10 +1000

    iteration 5

--- index.ts
+++ index.ts
@@ -1,38 +1,60 @@
+interface IAttackable {
+  alive: boolean
+  health: number;
+}
+
+export class Entity {
+  static _id: number = 0;
+
+  public id: number;
+
+  constructor() {
+    this.id = Entity._id++;
+  }
+}
+
 export class Faction {
   constructor(public name: string) {}
 }
 
-export class Character {
-  static _id: number = 0;
+export class Prop extends Entity implements IAttackable {
+  public alive: boolean = true;
 
+  constructor(public name: string, public health: number = 1000) {
+    super()
+  }
+}
+
+export class Character extends Entity implements IAttackable {
   public health: number = 1000;
   public level: number = 1;
   public alive: boolean = true;
-  public id: number;
   public range: number = 0;
   public factions: Faction[] = [];
 
   constructor() {
-    this.id = Character._id++;
+    super()
   }
 
   private isAllied(target: Character) {
     return this.factions.some((faction) => target.factions.includes(faction));
   }
 
-  public attack(target: Character, amount: number, distance: number = 0) {
-    if (this.id === target.id) return;
-
-    if (this.range < distance) return;
-
-    if (this.isAllied(target)) return;
-
+  public attack(target: IAttackable, amount: number, distance: number = 0) {
     let multipler = 1;
-
-    if (target.level - this.level >= 5) {
-      multipler = 0.5;
-    } else if (this.level - target.level >= 5) {
-      multipler = 1.5;
+    
+    if (this.range < distance) return;
+ 
+    if (target instanceof Character) {
+      if (this.id === target.id) return;
+      
+      if (this.isAllied(target)) return;
+  
+      if (target.level - this.level >= 5) {
+        multipler = 0.5;
+      } else if (this.level - target.level >= 5) {
+        multipler = 1.5;
+      }
     }
 
     target.health -= amount * multipler;
